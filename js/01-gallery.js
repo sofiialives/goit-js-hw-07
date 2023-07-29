c3d716583e7680d7a5b48f4aeb7be261b244a833
sofiialives <127687828+sofiialives@users.noreply.github.com>
2023-07-30 01:21:35 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery')
+
+const saveItems = galleryItems.map(item => 
+`<li class = "gallery__item">
+    <a class = "gallery__link" href = "${item.original}">
+    <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+    />
+    </a>S
+</li>`).join('')
+galleryList.insertAdjacentHTML('beforeend', saveItems)
+
+galleryList.addEventListener('click', event => {
+    event.preventDefault();
+    if (event.target.classList.contains('gallery__image')) {
+      const imageUrl = event.target.dataset.source;
+      const imageDesc = event.target.getAttribute('alt');
+      const instance = basicLightbox.create(`<img src="${imageUrl}" alt="${imageDesc}">`);
+      instance.show();
+    }
+})
+
 
 console.log(galleryItems);
